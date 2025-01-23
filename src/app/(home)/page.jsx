577ad33b0114b0131bfc53af92f6bafe42b77357
SergWebAcceptance <serg.web@acceptance.ltd>
2025-01-24 00:51:32 +0200

    added header

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import styles from "./page.module.scss";
 import useAuthStore from "@/stores/authStore";
 
 export default function Home() {
@@ -6,18 +7,20 @@ export default function Home() {
 
   return (
     <>
-      <p>Page</p>
-      <h1>Title</h1>
-      {user ? (
-        <div>
-          <h3>User Details:</h3>
-          <p>Email: {user.email}</p>
-          <p>Name: {user.firstName}</p>
-          <button onClick={logout}>Logout</button>
-        </div>
-      ) : (
-        <p>No user logged in</p>
-      )}
+      <div className={styles.page}>
+        <p>Page</p>
+        <h1>Title</h1>
+        {user ? (
+          <div>
+            <h3>User Details:</h3>
+            <p>Email: {user.email}</p>
+            <p>Name: {user.firstName}</p>
+            <button onClick={logout}>Logout</button>
+          </div>
+        ) : (
+          <p>No user logged in</p>
+        )}
+      </div>
     </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,96 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./Header.module.scss";
+import useAuthStore from "@/stores/authStore";
+import Link from "next/link";
+import FacebookIcon from "@/icons/socials/FacebookIcon";
+import XIcon from "@/icons/socials/XIcon";
+import InstagramIcon from "@/icons/socials/InstagramIcon";
+import CartIcon from "@/icons/CartIcon";
+
+const Header = () => {
+  const { user, logout } = useAuthStore();
+  const [scrolling, setScrolling] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const handleScroll = () => {
+    if (window.scrollY > 80) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 992);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className={styles.headerTop}>
+        <div className="_container">
+          <div className={styles.row}>
+            <div className={styles.col1}>
+              <div className={styles.soc}>
+                <Link href="#">
+                  <FacebookIcon />
+                </Link>
+                <Link href="#">
+                  <XIcon />
+                </Link>
+                <Link href="#">
+                  <InstagramIcon />
+                </Link>
+              </div>
+            </div>
+            <div className={styles.col2}>
+              <Link className={styles.cart} href={"/cart"}>
+                <CartIcon />
+              </Link>
+              {user ? (
+                <div className={styles.headAccount}>
+                  <Link href="/dashboard">Account</Link>
+                </div>
+              ) : (
+                <div className={styles.headAccount}>
+                  <Link href="/sign-up">Sign up</Link>|
+                  <Link href="/log-in">Log in</Link>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <header className={`${styles.header} ${scrolling ? styles.active : ""}`}>
+        <div className="_container">
+          <div className={styles.row}>
+            <Link href="/">
+              <img src="/images/logo.svg" />
+            </Link>
+            <nav className={styles.nav}>
+              <Link href="#">3D Modelling</Link>
+              <Link href="#">Animations</Link>
+              <Link href="#">Video Production</Link>
+              <Link href="#">UX  & UI</Link>
+              <Link href="#">Ideas</Link>
+              <Link href="#">Factory Tour</Link>
+              <Link href="#">Contact Us</Link>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
