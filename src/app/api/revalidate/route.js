5328a2fc8baf072b06592a259f81186268c84d68
SergWebAcceptance <serg.web@acceptance.ltd>
2025-02-04 18:28:19 +0200

    fixed revalidation

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -4,15 +4,18 @@ export async function POST(request) {
   try {
     const payload = await request.json();
 
-    // Лог payload для перевірки
     console.log("Payload received for revalidation:", payload);
 
-    // Перевірка моделі, яка потребує ревалідації
-    revalidateTag("products");
-    revalidateTag("ideas");
-    console.log("Cache revalidated for tag: home_data");
+    if (payload.tags && Array.isArray(payload.tags)) {
+      payload.tags.forEach((tag) => {
+        revalidateTag(tag);
+        console.log(`Cache revalidated for tag: ${tag}`);
+      });
+    } else {
+      console.warn("No valid tags found in payload.");
+    }
 
-    return new Response(null, { status: 204 }); // Повертаємо успішну відповідь
+    return new Response(null, { status: 204 });
   } catch (error) {
     console.error("Error in revalidation:", error);
     return new Response("Error processing request", { status: 500 });
