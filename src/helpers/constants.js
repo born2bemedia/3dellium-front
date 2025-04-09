2256188304b68ff48ab58a18c5c68c4c3d93ee53
SergWebAcceptance <serg.web@acceptance.ltd>
2025-04-09 18:46:58 +0300

    changed fb url

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -4,6 +4,6 @@ export const API_TOKEN = process.env.NEXT_PUBLIC_CMS_API_TOKEN;
 export const CACHE_TAG_PRODUCTS = "products";
 export const CACHE_TAG_IDEAS = "ideas";
 
-export const FACEBOOK_URL = "https://www.facebook.com/3dellium/";
+export const FACEBOOK_URL = "https://www.facebook.com/3delliumcom/";
 export const LINKEDIN_URL = "https://www.linkedin.com/company/3dellium/";
 export const INSTAGRAM_URL = "https://www.instagram.com/3dellium/";
