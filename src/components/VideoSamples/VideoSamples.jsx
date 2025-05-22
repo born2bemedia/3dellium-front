85dc0f96745a8025fa576c059997ca1563fda781
Daniil <dan.developer@born2be.media>
2025-05-22 18:15:45 +0300

    [feature/home-video-slider]: Home Video Slider (#19)

--- src/components/VideoSamples/VideoSamples.jsx
+++ src/components/VideoSamples/VideoSamples.jsx
@@ -6,39 +6,39 @@ const ReactPlayer = dynamic(() => import('react-player').then(m => m.default), {
   ssr: false,
 });
 
+import { EffectCards } from 'swiper/modules';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
-import { Autoplay, EffectCards } from 'swiper/modules';
 
 import st from './VideoSamples.module.scss';
 
+import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-import Image from 'next/image';
 
 const videos = [
   {
     title: 'Dynamic Happy Birthday Greeting Video',
     text: 'This lively, animated birthday greeting features colorful confetti, nostalgic photo transitions, and an interactive gift box. It also includes upbeat music, text animations, and a final cake surprise to celebrate the occasion with energy and fun. Perfect for a personalized birthday message.',
-    cover: '/images/video-production/cover1_home.webp',
-    video: '/videos/3Dellium_Vid_1.mp4',
+    cover: '/images/video-production/cover4_home.jpg',
+    video: '/videos/3Dellium_Vid_4.mp4',
   },
   {
     title: 'Personal Coach Promotion Video',
     text: 'This dynamic video promotes a personal coach’s services, showcasing expertise through engaging blog content, client testimonials, and motivational messages. Featuring energetic visuals of the coach in action, along with motivational text and seamless transitions, the video invites viewers to follow and engage for daily inspiration. It is perfect for promoting coaching services and building a personal brand.',
-    cover: '/images/video-production/cover2_home.webp',
-    video: '/videos/3Dellium_Vid_2.mp4',
+    cover: '/images/video-production/cover1_home.jpg',
+    video: '/videos/3Dellium_Vid_3.mp4',
   },
   {
     title: 'Freelance Developer Portfolio Video',
     text: 'An engaging video that highlights the skills, experience, and projects of a freelance developer. It showcases core competencies such as PHP, Java, Python, and JavaScript with interactive visuals, followed by real-world examples of projects like e-commerce solutions and app interfaces. The video concludes with a strong call to action, inviting potential clients to contact. Perfect for developers looking to showcase their portfolio and attract new business.',
-    cover: '/images/video-production/cover3_home.webp',
-    video: '/videos/3Dellium_Vid_3.mp4',
+    cover: '/images/video-production/cover3_home.jpg',
+    video: '/videos/3Dellium_Vid_2.mp4',
   },
   {
     title: 'Motivational Video for Social Networks',
     text: 'A calming and inspiring video designed to motivate and uplift audiences. It features serene visuals like sunrises, forest scenes, and peaceful imagery paired with reflective quotes. The video encourages self-care, action, and mindfulness, making it perfect for social media content that aims to inspire and engage viewers. The gentle call to action invites audiences to share positivity and follow for more daily inspiration.',
-    cover: '/images/video-production/cover4_home.webp',
-    video: '/videos/3Dellium_Vid_4.mp4',
+    cover: '/images/video-production/cover2_home.jpg',
+    video: '/videos/3Dellium_Vid_1.mp4',
   },
 ];
 
@@ -76,12 +76,9 @@ export function VideoSamples() {
   return (
     <Swiper
       loop
-      autoplay={{
-        delay: 5000,
-      }}
       effect="cards"
       className={st.slider}
-      modules={[EffectCards, Autoplay]}
+      modules={[EffectCards]}
       cardsEffect={{
         perSlideOffset: 8,
         slideShadows: false,
