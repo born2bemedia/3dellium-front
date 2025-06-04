6f14e640a1c0db6f1edf864bcf60f5931db63c03
daniil-b2be <dan.developer@born2be.media>
2025-06-04 09:27:47 +0300

    update social links

--- src/app/contact/components/InfoSecondColumn/InfoSecondColumn.jsx
+++ src/app/contact/components/InfoSecondColumn/InfoSecondColumn.jsx
@@ -8,7 +8,6 @@ import { SocialNetworks } from '../SocialNetworks/SocialNetworks';
 export function InfoSecondColumn() {
   return (
     <section className={st.columnLayout}>
-      
       <section>
         <div className={st.container}>
           <p className={st.label}>Registration address:</p>
@@ -18,11 +17,11 @@ export function InfoSecondColumn() {
           </p>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.64287500734!2d-0.43168912383165453!3d51.57478010577936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48766c5ef8135fab%3A0xe5b3326587a4c1a5!2sCollege%20House%2C%2017%20King%20Edwards%20Rd%2C%20Ruislip%20HA4%207AE!5e0!3m2!1sen!2suk!4v1747134864564!5m2!1sen!2suk"
-             width="336"
+            width="336"
             height="420"
             allowFullScreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </section>
